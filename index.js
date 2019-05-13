9f713954ca8674c95718dff554feda67e12e7cd6
Randy Harkediansa <randy.harkediansa@gmail.com>
2019-05-13 10:20:30 +0800

    Simple endpoint /url

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require("express");
 
 var app = express();
 
+app.get("/url", (req, res, next) => {
+    res.json( ["Tony", "Lisa", "Michael", "Ginger", "Food"] );
+});
+
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
